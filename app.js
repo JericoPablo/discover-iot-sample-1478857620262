9b635c081de81b953fbe2513bcb93b9c2654a303
Tugrul Duyar <Tugrul Duyar>
2016-11-15 16:25:05 +0100

    error handling

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var app = express();
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json()); // for parsing application/json
 var appEnv = cfenv.getAppEnv();
-
+var Client = require("ibmiotf");
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
@@ -60,7 +60,7 @@ app.get('/credentials', function(req, res) {
 
 
 app.get('/isDeviceMoving', function(req, res) {
-	 var Client = require("ibmiotf");
+	 
      var appClientConfig = {
       "org": basicConfig.org,
       "id": 'myapp',
@@ -75,7 +75,7 @@ app.get('/isDeviceMoving', function(req, res) {
 
     appClient.on("connect", function () {
 
-    appClient.subscribeToDeviceEvents("+","Tugrul","+","json");
+    aappClient.subscribeToDeviceEvents("myDeviceType","device01","+","json");
         console.log("Connected");
 
     });
